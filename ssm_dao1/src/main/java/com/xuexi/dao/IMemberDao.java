6c16658c182a9110b85284df5bd42b7b0c578ece
21321321312 <3123213>
2019-06-25 13:51:39 +0800

    添加注释

--- ssm_dao1/src/main/java/com/xuexi/dao/IMemberDao.java
+++ ssm_dao1/src/main/java/com/xuexi/dao/IMemberDao.java
@@ -3,6 +3,7 @@ package com.xuexi.dao;
 import com.xuexi.domain.Member;
 import org.apache.ibatis.annotations.Select;
 
+//dao层
 public interface IMemberDao {
 
     @Select("select * from member where id = #{memberId}")
